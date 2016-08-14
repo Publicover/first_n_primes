707f0ba81cc89d9ef3d65ef07ffb7fb46789511f
Publicover <jameslpublicover@gmail.com>
2016-08-14 11:31:29 -0400

    back up first_n_primes

---
+++ first_n_primes.rb
@@ -0,0 +1,29 @@
+prime_array = [2]
+counter = 2
+possible_prime = 3
+
+puts "\nHow many primes do you want to see?\n"
+n_primes = gets.chomp
+
+while prime_array.length < n_primes.to_i
+  loop do
+    if possible_prime % 2 == 0
+      possible_prime += 2
+      puts "#{possible_prime} isn't prime"
+    elsif counter == possible_prime
+      prime_array << possible_prime
+      possible_prime += 2
+      counter = 3
+    elsif possible_prime % counter != 0
+      counter += 1
+    else
+      counter = 3
+      possible_prime += 2
+      break
+    end
+  end
+end
+
+puts "\nArray of #{n_primes} elements coming your way:\n"
+print prime_array
+puts "\nUse this knowledge for the power of good."
